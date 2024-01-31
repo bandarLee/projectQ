98678f24096e21788cc57f65fede2e210167c05a
thlee <sheena1131@kakao.com>
2024-01-31 18:27:08 +0900

    1/31??

--- projectQ/Assets/02 Scripts/OneEyeM.cs
+++ projectQ/Assets/02 Scripts/OneEyeM.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class OneEyeMon : MonoBehaviour //프리팹 OneEyeMon(clone)에다가 삽입
 {
+
+
     public float time = 0;
+
+
+
+
+
+
+
+
+
     public float Destroytime = 0.8f;
 
     void Start()
