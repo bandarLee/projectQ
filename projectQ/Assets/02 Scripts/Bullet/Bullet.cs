cb214aae4403cd872a3765cf4755763ffe121607
DESKTOP-358CSLP\USER <hs.jo.sw.p@gmail.com>
2024-02-01 19:28:51 +0900

    Bullet 병합충돌오류

--- projectQ/Assets/02 Scripts/Bullet/Bullet.cs
+++ projectQ/Assets/02 Scripts/Bullet/Bullet.cs
@@ -7,9 +7,10 @@ public class Bullet : MonoBehaviour
     public enum BulletType
     {
         Normal = 1,
+        Fire = 2,
+
 
     }
-    public float Speed;
 
     public BulletType BType { get; internal set; }
 
@@ -17,11 +18,23 @@ public class Bullet : MonoBehaviour
 
     void Start()
     {
-        
+
     }
+    public float NormalBulletSpeed;
+    private Vector2 dir;
+    public void SetDirection(Vector2 direction)
+    {
+        this.dir = direction;
+    }
+
+
+   // 목표: 총알이 방향키를 누름에 따라 앞으로 나아가도록
 
     void Update()
     {
-        
+
+        transform.position = (Vector2)transform.position + dir * NormalBulletSpeed * Time.deltaTime;
+
     }
+
 }
