838ab5c44d7b4c77e0f927c24e4d618f83a9f69b
DESKTOP-358CSLP\USER <mirjeong6463@gmail.com>
2024-02-01 10:57:02 +0900

    수빈 11시 임시

--- projectQ/Assets/02 Scripts/Bullet/Bullet.cs
+++ projectQ/Assets/02 Scripts/Bullet/Bullet.cs
@@ -9,7 +9,7 @@ public class Bullet : MonoBehaviour
         Normal = 1,
 
     }
-    public float Speed;
+    public float NormalBulletSpeed;
     // 목표: 총알이 방향키를 누름에 따라 앞으로 나아가도록
 
     void Start()
@@ -19,6 +19,9 @@ public class Bullet : MonoBehaviour
 
     void Update()
     {
-        
+        float MoveH = Input.GetAxis("Horizontal");
+        float MoveV = Input.GetAxis("Vertical");
+
+        //Vector2 dir = new 
     }
 }
