21c430f5aa87c391fc2ffcf5e16abf618fce7766
thlee <sheena1131@kakao.com>
2024-02-01 10:37:55 +0900

    벽, 문 rigidbody 수정

---
+++ projectQ/Assets/02 Scripts/RoomManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public enum WallType
+    {
+        Default,
+        Left,
+        Right,
+        Top,
+        Bot
+    }
+    public  WallType walltype; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+}
