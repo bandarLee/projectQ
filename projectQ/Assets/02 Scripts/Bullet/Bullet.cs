2afaa64aaa45ef247085ed907247ba684c1fe71f
DESKTOP-358CSLP\USER <mirjeong6463@gmail.com>
2024-02-01 16:13:08 +0900

    1일차 - 플레이어가 움직이는 방향에 따라서 총알이 나가도록 하는 스크립트 추가

--- projectQ/Assets/02 Scripts/Bullet/Bullet.cs
+++ projectQ/Assets/02 Scripts/Bullet/Bullet.cs
@@ -10,7 +10,14 @@ public class Bullet : MonoBehaviour
 
     }
     public float NormalBulletSpeed;
-    // 목표: 총알이 방향키를 누름에 따라 앞으로 나아가도록
+    private Vector2 dir;
+    public void SetDirection(Vector2 direction)
+    {
+        this.dir = direction;
+    }
+
+
+   // 목표: 총알이 방향키를 누름에 따라 앞으로 나아가도록
 
     void Start()
     {
@@ -19,9 +26,9 @@ public class Bullet : MonoBehaviour
 
     void Update()
     {
-        float MoveH = Input.GetAxis("Horizontal");
-        float MoveV = Input.GetAxis("Vertical");
 
-        //Vector2 dir = new 
+        transform.position = (Vector2)transform.position + dir * NormalBulletSpeed * Time.deltaTime;
+
     }
+
 }
--- projectQ/Assets/02 Scripts/Player/PlayerFire.cs
+++ projectQ/Assets/02 Scripts/Player/PlayerFire.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
@@ -9,32 +10,87 @@ public class PlayerFire : MonoBehaviour
     public GameObject NormalBulletPrefab;
     public float ShootTimer;
     public float Cool_Time = 4;
-
     [Header("일반 공격 총구")]
     public List<GameObject> Muzzles;
+
+    public Animator playerAnimator;
 
 
 
     void Start()
     {
-        
+        ShootTimer = Cool_Time;
     }
 
     void Update()
     {
-        
-    }
-    private void Shooting()
-    {
-        ShootTimer = Cool_Time;
+        ShootTimer += Time.deltaTime;
+        AnimatorStateInfo stateInfo = playerAnimator.GetCurrentAnimatorStateInfo(0);
 
-        for (int i = 0; i < Muzzles.Count; i++)
+        if (ShootTimer >= Cool_Time && Input.GetKeyDown(KeyCode.Space))
         {
 
+            if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
+            {
+                Debug.Log("대각선공격1");
+                Shooting(new Vector2(1, 1));
+            }
+            else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
+            {
+                Debug.Log("대각선공격");
+
+                Shooting(new Vector2(-1, 1));
+            }
+            else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
+            {
+                Debug.Log("대각선공격");
+
+                Shooting(new Vector2(1, -1));
+            }
+            else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+            {
+                Debug.Log("대각선공격");
+
+                Shooting(new Vector2(-1, -1));
+            }
+            else if (Input.GetKey(KeyCode.UpArrow) || stateInfo.IsName("Back_Idle"))
+            {
+                Shooting(new Vector2(0, 1));
+            }
+            else if (Input.GetKey(KeyCode.DownArrow) || stateInfo.IsName("Front_Idle"))
+            {
+                Shooting(new Vector2(0, -1));
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow) || stateInfo.IsName("Left_Idle"))
+            {
+                Shooting(new Vector2(-1, 0));
+            }
+            else if (Input.GetKey(KeyCode.RightArrow) || stateInfo.IsName("Right_Idle"))
+            {
+                Shooting(new Vector2(1, 0));
+            }
         }
+    
 
 
 
 
+
+    }
+
+    // 총알을 발사하는 코드 
+    public void Shooting(Vector2 dir)
+    {
+        ShootTimer = 0;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < Muzzles.Count; i++)
+            {
+                GameObject normalBullet = Instantiate(NormalBulletPrefab, transform.position, Quaternion.identity);
+                normalBullet.transform.position = Muzzles[i].transform.position;
+                normalBullet.GetComponent<Bullet>().SetDirection(dir);
+            }
+
+        }
     }
 }
