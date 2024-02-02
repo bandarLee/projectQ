c1751de4f0f4304320cacbf0f8115c096e8e9de2
DESKTOP-358CSLP\USER <mirjeong6463@gmail.com>
2024-02-02 11:05:36 +0900

    [정수빈] 플레이어 데미지 구현중 2

--- projectQ/Assets/02 Scripts/Player/Player.cs
+++ projectQ/Assets/02 Scripts/Player/Player.cs
@@ -37,7 +37,7 @@ public class Player : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        if (collision.collider.CompareTag(""))
+        if (collision.collider.CompareTag("OneEyeEnemy"))
         {
             PlayerHealth--;
         }
