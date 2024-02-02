b22aa1f30f055f6f161dc8b9d76e95259f1e5f02
thlee <sheena1131@kakao.com>
2024-02-02 17:37:22 +0900

    [이태환] 모든 방별, 그 방 들어갔을 때 그방에 있는 몬스터가 나타나는 코드

--- projectQ/Assets/02 Scripts/DetailRoomManager.cs
+++ projectQ/Assets/02 Scripts/DetailRoomManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class DetailRoomManager : MonoBehaviour
 {
+    public Rect roomRect;
+
 
     void Start()
     {
+        roomRect = Rect.MinMaxRect(this.transform.position.x - 7, this.transform.position.y - 3, this.transform.position.x + 7, this.transform.position.y + 3);
 
     }
 
     void Update()
     {
-        
+
     }
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -21,9 +24,26 @@ public class DetailRoomManager : MonoBehaviour
         {
             foreach (OneEye oneeye in oneeyes)
             {
-                if (!oneeye.gameObject.activeInHierarchy)
+          
+                if (!oneeye.gameObject.activeInHierarchy && roomRect.Contains(oneeye.gameObject.transform.position))
+                        {
+                            oneeye.gameObject.SetActive(true);
+                        }
+
+            }
+
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        OneEye[] oneeyes = RoomManager.Instance.oneeyes;
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach (OneEye oneeye in oneeyes)
+            {
+                if (oneeye.gameObject.activeInHierarchy && !roomRect.Contains(oneeye.gameObject.transform.position))
                 {
-                    oneeye.gameObject.SetActive(true);
+                    oneeye.gameObject.SetActive(false);
                 }
             }
         }
