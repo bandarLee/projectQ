5cb156feef89490223d3ded148265193df7fe381
DESKTOP-358CSLP\USER <hs.jo.sw.p@gmail.com>
2024-02-03 18:17:05 +0900

    [조희수] Virus 본체 작업

--- projectQ/Assets/02 Scripts/Enemy/Virus.cs
+++ projectQ/Assets/02 Scripts/Enemy/Virus.cs
@@ -18,29 +18,28 @@ public class Virus : MonoBehaviour // " Follow 타입 "
     public float Movespeed;
     // 위아래로 흔들리는 속도 
     public float Movespeed2;
-    // 플레이어 객체
-    public GameObject _target;
 
-    // 자기 복제 프리팹 // -> 잔상말고 똑같은 아이 자가복제 할 것임
+    /*// 플레이어 객체
+    public GameObject _target;*/
+
+    // 위아래 흔들림 주기
+    public float swingtime = 0.6f;
+    // 위아래 흔들림 상태 변경까지 남은 시간
+    public float time2;
+
+
+    // 자기 복제 프리팹 
     public GameObject shadowPrefab;
     // 자기 복제 생성 지연 여부
     public bool isCloneDelay;
     // 자기 복제 생성 주기
     public float Respawntime = 2f;
-
-    // 위아래 흔들림 주기
-    public float swingtime = 0.6f;
-
     // 자기 복제 생성까지 남은 시간
     public float time;
-
-    // 위아래 흔들림 상태 변경까지 남은 시간
-    public float time2;
 
 
     void Start()
     {
-
     }
 
     void Update()
@@ -52,7 +51,7 @@ public class Virus : MonoBehaviour // " Follow 타입 "
         Vector2 dir2 = new Vector2(0, -0.1f);
 
         //플레이어를 향하는 방향
-        Vector2 dir3 = (_target.transform.position - this.transform.position).normalized;
+        Vector2 dir3 = (Player.Instance.transform.position - this.transform.position).normalized;
 
         // 자기 복제 생성 주기를 계산
         if (isCloneDelay)
@@ -73,21 +72,21 @@ public class Virus : MonoBehaviour // " Follow 타입 "
             time2 = 0.0f;
         }
 
-
         float angle = Mathf.Atan2(dir3.y, dir3.x) * Mathf.Rad2Deg;
         angle += 90f;  // 몬스터가 플레이어를 둘러싸도록 각도를 조절
         dir3 = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
-
+        
+        // 플레이어를 향해 이동
         transform.position += (Vector3)(dir3 * Movespeed * Time.deltaTime);
 
 
         //위아래 흔들림
-        if (time2 < 0.4f) //위로 이동
+        if (time2 < 0.3f) //위로 이동
         {
             transform.position += (Vector3)(dir1 * Movespeed2 * Time.deltaTime);
 
         }
-        if (time2 > 0.4f) //아래로 이동
+        if (time2 > 0.3f) //아래로 이동
         {
             transform.position += (Vector3)(dir2 * Movespeed2 * Time.deltaTime);
 
@@ -99,10 +98,8 @@ public class Virus : MonoBehaviour // " Follow 타입 "
     void OnCollisionStay2D(Collision2D collision)
     {
         // 플레이어와 충돌했는지 확인
-        if (collision.gameObject == _target)
+        if (collision.collider.CompareTag("Player"))
         {
-            //Debug.Log("충돌!");
-
             // 복제 생성 여부와 현재 복제 개수를 확인
             if (!isCloneDelay)
             {
@@ -111,6 +108,58 @@ public class Virus : MonoBehaviour // " Follow 타입 "
                 enemy.transform.position = this.transform.position;
                 isCloneDelay = true;
             }
+
+            Player.Instance.PlayerHealth -= 1f;
+        }
+
+        // 플레이어의 공격을 받았을 때 죽는다
+        if (collision.collider.CompareTag("Bullet")) //enemy와 총알이 부딪혔을 때 
+        {
+            Bullet bullet = collision.collider.GetComponent<Bullet>();
+            if (bullet.Btype == Bullet.BulletType.Normal) //enum
+            {
+                Health -= 1;
+            }
+
+            // 적의 체력이 끝
+            if (Health <= 0)
+            {
+                gameObject.SetActive(false);
+                MakeItem();
+            }
+        }
+    }
+
+    public void MakeItem()
+    {
+        // 목표: 25% 확률로 다음 층으로 넘어갈 수 있는 카드키, 머니, 체력, 스피드 아이템(확률넣기)
+        if (Random.Range(0, 100) < 25)
+        {
+            // -다음 층으로 넘어갈 수 있는 카드키 만들고
+            GameObject item_CardKey = Instantiate(ItemPrefab_CardKey);
+            // -위치를 나의 위치로 수정
+            item_CardKey.transform.position = this.transform.position;
+        }
+        else if (Random.Range(0, 100) < 50)
+        {
+            // -머니주는 아이템 만들고
+            GameObject item_Money = Instantiate(ItemPrefab_Money);
+            // -위치를 나의 위치로 수정
+            item_Money.transform.position = this.transform.position;
+        }
+        else if (Random.Range(0, 100) < 75)
+        {
+            // -체력 아이템 만들고
+            GameObject item_Health = Instantiate(ItemPrefab_Health);
+            // -위치를 나의 위치로 수정
+            item_Health.transform.position = this.transform.position;
+        }
+        else
+        {
+            // -스피드 아이템 만들고
+            GameObject item_Speed = Instantiate(ItemPrefab_Speed);
+            // -위치를 나의 위치로 수정
+            item_Speed.transform.position = this.transform.position;
         }
     }
 }
