1ffade0af1969c04ef8e1b9e1b177fd17c864a68
DESKTOP-358CSLP\USER <hs.jo.sw.p@gmail.com>
2024-02-05 14:44:52 +0900

    [조희수] 장애물과 부딪혔을 때 enemyBullet 삭제

--- projectQ/Assets/02 Scripts/Enemy/EnemyBullet.cs
+++ projectQ/Assets/02 Scripts/Enemy/EnemyBullet.cs
@@ -60,7 +60,7 @@ public class EnemyBullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Player") || collision.collider.CompareTag("Environment")) 
+        if (collision.collider.CompareTag("Player") || collision.collider.CompareTag("Environment") ) 
         {
             Destroy(this.gameObject);
         }
