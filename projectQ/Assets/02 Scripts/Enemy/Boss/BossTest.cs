125355ae9de19650157f9a497cc6887929e9a80d
DESKTOP-358CSLP\USER <hs.jo.sw.p@gmail.com>
2024-02-07 11:34:15 +0900

    [조희수] 보스 다리 구현중

--- projectQ/Assets/02 Scripts/Enemy/Boss/BossTest.cs
+++ projectQ/Assets/02 Scripts/Enemy/Boss/BossTest.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class BossTest : MonoBehaviour
 {
-    private float rotateSpeed = 30f;
+    private float rotateSpeed = 20f;
+    private float acceleration = 1.01f; // 가속도
 
     private float currentAngle = 0f; 
     private int rotateDirection = 1; 
@@ -23,19 +24,26 @@ public class BossTest : MonoBehaviour
     }
     void Rotate()
     {
+        // 현재 각도가 20도 이상이고, 방향이 내려가는 방향일 때 가속도를 적용
+        if (currentAngle >= 20f && rotateDirection == -1)
+        {
+            StartCoroutine(AccelerateRightarm());
+        }
+
         currentAngle += rotateSpeed * Time.deltaTime * rotateDirection;
         currentAngle = Mathf.Clamp(currentAngle, 0f, 40f);
 
         if (currentAngle >= 40f || currentAngle <= 0f)
         {
-            if (currentAngle >= 20f || currentAngle <= 0f)
-            {
-
-            }
-                rotateDirection *= -1;
+            rotateSpeed = 20f;
+            rotateDirection *= -1;
         }
 
         transform.rotation = Quaternion.Euler(0, 0, currentAngle);
-
+    }
+    IEnumerator AccelerateRightarm()
+    {
+        yield return new WaitForSeconds(0.1f);
+        rotateSpeed *= acceleration;
     }
 }
---
+++ projectQ/Assets/02 Scripts/Enemy/Boss/LeftArm.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftArm : MonoBehaviour
+{
+    private float rotateSpeed = 30f;
+    private float acceleration = 1.01f; // 가속도
+
+    private float currentAngle = 0f;
+    private int rotateDirection = -1; // 초기 회전 방향 반대
+
+    void Start()
+    {
+        Rotate();
+
+    }
+
+
+    void Update()
+    {
+        Rotate();
+
+    }
+    void Rotate()
+    {
+
+        // 현재 각도가 -20도 이상이고, 방향이 내려가는 방향일 때 가속도를 적용
+        if (currentAngle >= -20f && rotateDirection == 1)
+        {
+            StartCoroutine(AccelerateLeftarm());
+
+        }
+
+        currentAngle += rotateSpeed * Time.deltaTime * rotateDirection;
+        currentAngle = Mathf.Clamp(currentAngle, -40f, 0f); // 회전 범위를 반대로 설정
+
+        if (currentAngle <= -40f || currentAngle >= 0f)
+        {
+            rotateSpeed = 30f;
+            rotateDirection *= -1;
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, currentAngle);
+    }
+     IEnumerator AccelerateLeftarm()
+    {
+        yield return new WaitForSeconds(0.1f);
+        rotateSpeed *= acceleration;
+
+
+    }
+
+}
