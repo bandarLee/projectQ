4981e6243f6b90bf457f294688c8aba01bcacb44
DESKTOP-358CSLP\USER <mirjeong6463@gmail.com>
2024-02-07 21:39:37 +0900

    [정수빈] 로비 수정중

---
+++ projectQ/Assets/02 Scripts/LobbyManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Scene1Timer : MonoBehaviour
+{
+    public GameObject Image1;
+    public GameObject Image2;
+    public GameObject Image3;
+    public GameObject Image4;
+    public GameObject Image5;
+    private CanvasGroup canvasGroup1;
+    private CanvasGroup canvasGroup2;
+    private CanvasGroup canvasGroup3;
+    private CanvasGroup canvasGroup4;
+    private CanvasGroup canvasGroup5;
+    public float fadeDuration = 1f; // 페이드 지속 시간
+ 
+
+
+    void Awake()
+    {
+        Image1.SetActive(false);
+        Image2.SetActive(false);
+        Image3.SetActive(false);
+        Image4.SetActive(false);
+        Image5.SetActive(false);
+        canvasGroup1 = Image1.GetComponent<CanvasGroup>();
+        canvasGroup2 = Image2.GetComponent<CanvasGroup>();
+        canvasGroup3 = Image3.GetComponent<CanvasGroup>();
+        canvasGroup4 = Image4.GetComponent<CanvasGroup>();
+        canvasGroup5 = Image5.GetComponent<CanvasGroup>();
+    }
+    void Start()
+    {
+        StartCoroutine(EndGameSequence());
+
+
+    }
+
+
+
+
+
+    IEnumerator EndGameSequence()
+    {
+
+        Image1.SetActive(true);
+        StartCoroutine(FadeCanvasGroup(canvasGroup1, 0f, 1f, fadeDuration));
+        yield return new WaitForSeconds(4);
+        Image2.SetActive(true);
+        StartCoroutine(FadeCanvasGroup(canvasGroup2, 0f, 1f, fadeDuration));
+        yield return new WaitForSeconds(4);
+        Image1.SetActive(false);
+        Image3.SetActive(true);
+        StartCoroutine(FadeCanvasGroup(canvasGroup3, 0f, 1f, fadeDuration));
+        yield return new WaitForSeconds(4);
+        Image2.SetActive(false);
+        Image4.SetActive(true);
+        StartCoroutine(FadeCanvasGroup(canvasGroup4, 0f, 1f, fadeDuration));
+        yield return new WaitForSeconds(5);
+        Image3.SetActive(false);
+        //SceneManager.LoadScene("Scene3");
+    }
+
+    private IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float duration)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            cg.alpha = Mathf.Lerp(start, end, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        cg.alpha = end;
+    }
+}
