c5de71801ef81c1f695bfb1f90cd225a5912c74a
DESKTOP-358CSLP\USER <hs.jo.sw.p@gmail.com>
2024-02-07 16:41:32 +0900

    [조희수] 보스 움직임 구현중

--- projectQ/Assets/02 Scripts/Enemy/Boss/BossTest.cs
+++ projectQ/Assets/02 Scripts/Enemy/Boss/BossTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BossTest : MonoBehaviour
 {
-    private float rotateSpeed = 20f;
+    private float rotateSpeed = 30f;
     private float acceleration = 1.01f; // 가속도
 
     private float currentAngle = 0f; 
@@ -25,18 +25,18 @@ public class BossTest : MonoBehaviour
     void Rotate()
     {
         // 현재 각도가 20도 이상이고, 방향이 내려가는 방향일 때 가속도를 적용
-        if (currentAngle >= 20f && rotateDirection == -1)
+        if (currentAngle >= 5f && rotateDirection == -1)
         {
             StartCoroutine(AccelerateRightarm());
         }
 
         currentAngle += rotateSpeed * Time.deltaTime * rotateDirection;
-        currentAngle = Mathf.Clamp(currentAngle, 0f, 40f);
+        currentAngle = Mathf.Clamp(currentAngle, 0f, 100f);
 
-        if (currentAngle >= 40f || currentAngle <= 0f)
+        if (currentAngle >= 30f || currentAngle <= 0f)
         {
             rotateSpeed = 20f;
-            rotateDirection *= -1;
+            rotateDirection *= -1; //해줄 때 탄막 생성 (빵) 
         }
 
         transform.rotation = Quaternion.Euler(0, 0, currentAngle);
--- projectQ/Assets/02 Scripts/Enemy/Boss/RightLeg.cs
+++ projectQ/Assets/02 Scripts/Enemy/Boss/RightLeg.cs
@@ -2,34 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeftLeg : MonoBehaviour
+public class RightLeg : MonoBehaviour
 {
-    private float rotateSpeed = 30f;
+    private float rotateSpeed = 20f;
     private float acceleration = 1.01f; // 가속도
 
     private float currentAngle = 0f;
-    private int rotateDirection = -1; // 초기 회전 방향 반대
+    private int rotateDirection = 1;
 
-    private float moveSpeed = 3f;
+    private float moveSpeed = 2.5f;
     private float moveDistance = 1.5f; // 이동 거리
 
     private float currentDistance = 0f;
-    private int moveDirection = -1; // 초기 이동 방향 아래로
+    private int moveDirection = -1; 
 
     void Start()
     {
         Rotate();
-        Move();
-
+        //Move();
     }
 
 
     void Update()
     {
         Rotate();
-        Move();
+        //Move();
     }
-    void Move()
+   /* void Move()
     {
         currentDistance += moveSpeed * Time.deltaTime * moveDirection;
         currentDistance = Mathf.Clamp(currentDistance, -moveDistance, 0f); // 이동 범위를 설정
@@ -42,14 +41,13 @@ public class LeftLeg : MonoBehaviour
         // Y축을 기준으로 이동
         transform.position = new Vector3(transform.position.x, currentDistance, transform.position.z);
     }
-
+   */
     void Rotate()
     {
-
         // 현재 각도가 -15도 이상이고, 방향이 내려가는 방향일 때 가속도를 적용
         if (currentAngle >= -15f && rotateDirection == 1)
         {
-            StartCoroutine(AccelerateLeftarm());
+            StartCoroutine(AccelerateRightarm());
 
         }
 
@@ -62,9 +60,9 @@ public class LeftLeg : MonoBehaviour
             rotateDirection *= -1;
         }
 
-        transform.rotation = Quaternion.Euler(0, 0, currentAngle);
+        transform.rotation = Quaternion.Euler(0, 180, currentAngle);
     }
-    IEnumerator AccelerateLeftarm()
+    IEnumerator AccelerateRightarm()
     {
         yield return new WaitForSeconds(0.1f);
         rotateSpeed *= acceleration;
