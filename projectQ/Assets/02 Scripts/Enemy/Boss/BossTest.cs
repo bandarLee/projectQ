0fcad32f7b81090249cb25716ba8bc3887658697
DESKTOP-358CSLP\USER <hs.jo.sw.p@gmail.com>
2024-02-08 11:52:54 +0900

    [조희수] 보스팔다리몸통얼굴 파괴

--- projectQ/Assets/02 Scripts/Enemy/Boss/BossTest.cs
+++ projectQ/Assets/02 Scripts/Enemy/Boss/BossTest.cs
@@ -5,12 +5,6 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class BossTest : MonoBehaviour
 {
-    public BossBullet bossBullet;
-
-    public float Health = 5;
-
-    public ItemSpawner itemspawner;
-
     private float rotateSpeed = 30f;
     private float acceleration = 1.01f; // 가속도
 
@@ -104,24 +98,6 @@ public class BossTest : MonoBehaviour
         }*//*
     }*/
 
-    public void OnCollisionEnter2D(Collision2D collision) 
-    { 
-    // 플레이어의 공격을 받았을 때 죽는다
-        if (collision.collider.CompareTag("Bullet")) //enemy와 총알이 부딪혔을 때 
-        {
-
-            Health -= Player.Instance.BulletPower;
-
-
-            // 총알 삭제
-            collision.collider.gameObject.SetActive(false);
-
-            // 적의 체력이 끝
-            if (Health <= 0)
-            {
-                gameObject.SetActive(false);
-            }
-        }
-    }
+    
 
 }
---
+++ projectQ/Assets/02 Scripts/Enemy/Boss/BossTest1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTest1 : MonoBehaviour
+{
+    public BossBullet bossBullet;
+
+    public float Health = 5;
+
+    public ItemSpawner itemspawner;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 플레이어의 공격을 받았을 때 죽는다
+        if (collision.collider.CompareTag("Bullet")) //enemy와 총알이 부딪혔을 때 
+        {
+
+            Health -= Player.Instance.BulletPower;
+
+
+            // 총알 삭제
+            collision.collider.gameObject.SetActive(false);
+
+            // 적의 체력이 끝
+            if (Health <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
